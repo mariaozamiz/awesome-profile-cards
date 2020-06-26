bdf452b341a18853e210da6ee3e68a3b830d0bac
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-06-26 12:44:33 +0200

    add name and position input listeners

--- src/js/01-design.js
+++ src/js/01-design.js
@@ -1,14 +1,16 @@
-'use strict';
-console.log('partial 1');
+"use strict";
+console.log("partial 1");
 
 //selectors
 
-const inputName = document.querySelector('.input__name');
+const inputName = document.querySelector(".input__name");
+const inputPosition = document.querySelector(".input__position");
 
 function addInfo(event) {
-    const inputValue = event.currentTarget.value;
-    console.log(inputValue);
+  const inputValue = event.target.value;
+  document.querySelector(".js-" + event.target.name).innerHTML = inputValue;
+  console.log(".js-" + event.target.name);
 }
 
-inputName.addEventListener('keyup', addInfo);
-// console.log(addInfo());
+inputName.addEventListener("keyup", addInfo);
+inputPosition.addEventListener("keyup", addInfo);
