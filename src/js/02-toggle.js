bf440c750514f1ba40c9fc39686193b1f67f8713
Analo-pez <ana.ts.lopez@gmail.com>
2020-06-29 18:56:11 +0200

    add collapsable section fill

--- src/js/02-toggle.js
+++ src/js/02-toggle.js
@@ -1,1 +1,10 @@
-console.log("partial 2");
+console.log('partial 2');
+
+const collContent = document.querySelector('.js-contentFill');
+const collButton = document.querySelector('.js-coll');
+
+function collForm(event) {
+  collContent.classList.toggle('content');
+}
+
+collButton.addEventListener('click', collForm);
