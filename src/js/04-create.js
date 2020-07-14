e1f3faa1b9e838f9aaaa04a7017188e0371f187b
Analo-pez <ana.ts.lopez@gmail.com>
2020-07-14 12:31:04 +0200

    create fetch

--- src/js/04-create.js
+++ src/js/04-create.js
@@ -2,6 +2,19 @@
 
 const createCardButton = document.querySelector('.js-createcard__button');
 const shareContainer = document.querySelector('.js-share__section');
+const url = 'https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/';
+
+const testObject = {
+  palette: 1,
+  name: "María García",
+  job: "Front-end developer",
+  phone: "+34 666666666",
+  email: "mariagar@example.com",
+  linkedin: "mariagar",
+  github: "mariagar",
+  photo: "data:image/png;base64,2342ba..."
+};
+
 
 function updateButtonStatus() {
   if (data.name === '') {
@@ -11,26 +24,32 @@ function updateButtonStatus() {
     createCardButton.classList.remove('createcard__button--inactive');
     createCardButton.disabled = false;
   }
-}
+};
 
 function createCard() {
   event.preventDefault();
-  // fetch(url, {
-  //     method: 'POST', // or 'PUT'
-  //     body: JSON.stringify(data), // data can be `string` or {object}!
-  //     headers:{
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }).then(res => res.json())
-  //   .catch(error => console.error('Error:', error))
-  // .then(data => {
-  //     console.log('Success:', data)
-  //     document.querySelector('.response__url').innerHTML = data.algo
-  //     document.querySelector('.js-twitter-btn').href = 'https://twitter.....?url=' + data.algo
-  //   //
-  // });
-  //   createCardButton.classList.add('createcard__button--inactive');
-  //   shareContainer.classList.toggle('content');
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(testObject),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }).then(resp => resp.json())
+    .then(result => getURL(result))
+    .catch(error => console.error('Error:', error));
+
+
+  const twitterBtn = document.querySelector('.js-share__button');
+  const notification = document.querySelector('.notification');
+  const responseURL = document.querySelector('.js-apiLink');
+
+  function getURL(result) {
+    if (result.success) {
+      responseURL.innerHTML = `<a href="${result.cardURL}" target="_blank">${result.cardURL}</a>`;
+      notification.classList.remove('content')
+      console.log(result.cardURL)
+    }
+  }
 }
 
 createCardButton.addEventListener('click', createCard);
