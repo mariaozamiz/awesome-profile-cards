014b0a863052a813601e588db93ad65f1c25f82a
Maria Ozamiz <ozamiz.m@gmail.com>
2021-01-04 21:23:42 +0100

    save taken picture

--- src/js/03-image.js
+++ src/js/03-image.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const fr = new FileReader();
 const uploadBtn = document.querySelector('.js__profile-trigger');
 const fileField = document.querySelector('.js__profile-upload-btn');
 const profileImage = document.querySelector('.js__profile-image');
@@ -33,90 +32,59 @@ function startPhotoBooth() {
   takePicButton.classList.remove('content');
 }
 
-function savePicture() {
-  console.log('¡patata!');
+function takePicture() {
+  //define size
   const width = video.videoWidth;
   const height = video.videoHeight;
   canvas.width = width;
   canvas.height = height;
+  //flip image
   ctx.translate(width, 0);
   ctx.scale(-1, 1);
+  //save image data
   ctx.drawImage(video, 0, 0, width, height);
-  const data = canvas.toDataURL('image/jpeg');
-  profileImage.style.backgroundImage = `url(${data})`;
-  profilePreview.style.backgroundImage = `url(${data})`;
+  const imageData = canvas.toDataURL('image/jpeg');
+  //style
+  profileImage.style.backgroundImage = `url(${imageData})`;
+  profilePreview.style.backgroundImage = `url(${imageData})`;
   video.classList.add('content');
   takePicButton.classList.add('content');
+  //stop camera
   cameraStream.getTracks()[0].stop();
   cameraStream = false;
+  saveImage(imageData);
 }
 
-// canvas.translate(width, 0);
-// canvas.scale(-1, 1);
-// this.canvasContext.drawImage(image, 0, 0);
+function saveImage(imageData) {
+  data.photo = imageData;
+  saveInfo();
+  writeImage();
+}
 
-/**
- * Recoge el archivo añadido al campo de tipo "file"
- * y lo carga en nuestro objeto FileReader para que
- * lo convierta a algo con lo que podamos trabajar.
- * Añade un listener al FR para que ejecute una función
- * al tener los datos listos
- * @param {evento} e
- */
-
-let imgcurrent = '';
-function getImage(e) {
+function uploadImage(e) {
   const myFile = e.currentTarget.files[0];
-  fr.addEventListener('load', writeImage);
+  const fr = new FileReader();
+  fr.addEventListener('load', () => saveImage(fr.result));
   fr.readAsDataURL(myFile);
 }
 
-/**
- * Una vez tenemos los datos listos en el FR podemos
- * trabajar con ellos ;)
- */
 function writeImage() {
-  /* En la propiedad `result` de nuestro FR se almacena
-   * el resultado. Ese resultado de procesar el fichero que hemos cargado
-   * podemos pasarlo como background a la imagen de perfil y a la vista previa
-   * de nuestro componente.
-   */
-  if (data.photo !== '' || fr.result) {
-    profileImage.style.backgroundImage = `url(${data.photo || fr.result})`;
+  if (data.photo !== '') {
+    profileImage.style.backgroundImage = `url(${data.photo})`;
     profilePreview.style.backgroundImage = `url(${data.photo})`;
-
-    if (fr.result) {
-      data.photo = fr.result;
-    }
-
-    saveInfo();
   }
 }
 
 function resetImage() {
-  /* En la propiedad `result` de nuestro FR se almacena
-   * el resultado. Ese resultado de procesar el fichero que hemos cargado
-   * podemos pasarlo como background a la imagen de perfil y a la vista previa
-   * de nuestro componente.
-   */
   profileImage.style.backgroundImage = `url('assets/images/profile-image.jpg')`;
   profilePreview.style.backgroundImage = `url('assets/images/profile-image.jpg')`;
 }
 
-/**
- * Genera un click automático en nuesto campo de tipo "file"
- * que está oculto
- */
 function fakeFileClick() {
   fileField.click();
 }
 
-/**
- * Añadimos los listeners necesarios:
- * - al botón visible para generar el click automático
- * - al campo oculto para cuando cambie su value
- */
 uploadBtn.addEventListener('click', fakeFileClick);
-fileField.addEventListener('change', getImage);
+fileField.addEventListener('change', uploadImage);
 cameraButton.addEventListener('click', startPhotoBooth);
-takePicButton.addEventListener('click', savePicture);
+takePicButton.addEventListener('click', takePicture);
